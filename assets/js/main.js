b78455b845b2fdf1f95408e84a63cc71933daa33
Rayzr522 <rayzr522@gmail.com>
2019-07-13 17:30:09 -0400

    Improved help command

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,6 @@ window.addEventListener('load', function () {
     input.focus();
 
     document.addEventListener('keydown', event => {
-        console.log(event.keyCode);
         if (event.keyCode === 13) {
             handleCommand(input.value);
             input.value = '';
@@ -56,8 +55,9 @@ function handleCommand(input) {
     }
 }
 
-function registerCommand(name, handler) {
+function registerCommand(name, description, handler) {
     let names = [].concat(name).map(i => i.toLowerCase());
+    handler.description = description;
     names.forEach(i => commands[i] = handler);
 }
 
@@ -72,17 +72,17 @@ function mapToNumbers(args) {
 }
 
 // Math
-registerCommand(['add', '+'], args => mapToNumbers(args).reduce((mem, next) => mem + next, 0));
-registerCommand(['subtract', '-'], args => {
+registerCommand(['add', '+'], 'adds two or more numbers', args => mapToNumbers(args).reduce((mem, next) => mem + next, 0));
+registerCommand(['subtract', '-'], 'subtracts two or more numbers', args => {
     let numbers = mapToNumbers(args);
     let first = numbers.shift();
     return numbers.reduce((mem, next) => mem - next, first);
 });
-registerCommand(['multiply', '*'], args => mapToNumbers(args).reduce((mem, next) => mem * next, 1));
+registerCommand(['multiply', '*'], 'multiplies two or more numbers', args => mapToNumbers(args).reduce((mem, next) => mem * next, 1));
 
 
 // Filesystem
-registerCommand('cd', args => {
+registerCommand('cd', 'changes the current working directory to a different folder', args => {
     checkArgs(args, 1, 'cd <folder>');
 
     let path = sys.fs._resolve(args[0]);
@@ -97,13 +97,17 @@ registerCommand('cd', args => {
     return path;
 });
 
-registerCommand('pwd', () => sys.fs.pwd || '/');
-registerCommand('ls', args => sys.fs.readDir(args[0] || '').map(node => node.name + (node.type === 'd' ? '/' : '')).join('\n'));
-registerCommand('touch', args => checkArgs(args, 1, 'touch <file>') || sys.fs.touch(args[0]));
-registerCommand('mkdir', args => checkArgs(args, 1, 'mkdir <folder>') || sys.fs.mkdir(args[0]));
-registerCommand('cat', args => checkArgs(args, 1, 'cat <file>') || sys.fs.readFile(args[0]));
-registerCommand('write', args => checkArgs(args, 1, 'write <file> [contents]') || sys.fs.writeFile(args[0], args.slice(1).join(' ') + '\n'));
-registerCommand('append', args => checkArgs(args, 1, 'append <file> [contents]') || sys.fs.appendFile(args[0], args.slice(1).join(' ') + '\n'));
-registerCommand('rm', args => checkArgs(args, 1, 'rm <path>') || sys.fs.delete(args[0]));
+registerCommand('pwd', 'shows the current working directory', () => sys.fs.pwd || '/');
+registerCommand('ls', 'lists all files in the current directory', args => sys.fs.readDir(args[0] || '').map(node => node.name + (node.type === 'd' ? '/' : '')).join('\n'));
+registerCommand('touch', 'creates or updates the timestamp for the given file', args => checkArgs(args, 1, 'touch <file>') || sys.fs.touch(args[0]));
+registerCommand('mkdir', 'makes a new directory', args => checkArgs(args, 1, 'mkdir <folder>') || sys.fs.mkdir(args[0]));
+registerCommand('cat', 'outputs the contents of a file', args => checkArgs(args, 1, 'cat <file>') || sys.fs.readFile(args[0]));
+registerCommand('write', 'writes the given contents to a file', args => checkArgs(args, 1, 'write <file> [contents]') || sys.fs.writeFile(args[0], args.slice(1).join(' ') + '\n'));
+registerCommand('append', 'appends the given contents to a file', args => checkArgs(args, 1, 'append <file> [contents]') || sys.fs.appendFile(args[0], args.slice(1).join(' ') + '\n'));
+registerCommand('rm', 'removes a file', args => checkArgs(args, 1, 'rm <path>') || sys.fs.delete(args[0]));
 
-registerCommand('help', () => Object.keys(commands).join('\n'))
+registerCommand('help', 'shows a list of all commands', () => {
+    let keys = Object.keys(commands).sort();
+    let longest = keys.reduce((mem, next) => Math.max(mem, next.length), 0);
+    return keys.map(name => `${name}${' '.repeat(longest - name.length)} : ${commands[name].description}`).join('\n')
+});
