fe4fdb0570a9b7772afa708712f83aeda126a331
rayzr522 <rayzr522@gmail.com>
2023-05-02 16:07:40 -0400

    convert fs to classes

--- src/fs.js
+++ src/fs.js
@@ -1,11 +1,3 @@
-function FSNode(name, type) {
-  Object.defineProperty(this, '_id', { value: FSNode.nextID++ })
-  this.name = name
-  this.type = type
-}
-
-FSNode.nextID = 0
-
 function deserializeDir(dir) {
   let folder = new FolderNode(dir.name)
 
@@ -26,80 +18,86 @@ function deserializeFile(file) {
   return new FileNode(file.name, file.content)
 }
 
-FSNode.deserialize = function (serialized) {
-  if (!serialized) return new FolderNode('')
+class FSNode {
+  static nextID = 0
 
-  return deserializeDir(serialized)
-}
-
-FSNode.prototype.getPath = function () {
-  let out = ''
-  let next = this
-
-  while (next) {
-    out = next.name + '/' + out
-    next = next.parent
+  constructor(name, type) {
+    this._id = FSNode.nextID++
+    this.name = name
+    this.type = type
   }
 
-  return out
-}
+  static deserialize(serialized) {
+    if (!serialized) return new FolderNode('')
 
-function FolderNode(name) {
-  // super()
-  FSNode.call(this, name, 'd')
-
-  this.children = []
-}
-
-FolderNode.prototype = new FSNode()
-
-FolderNode.prototype.get = function (name) {
-  return this.children.find((node) => node.name === name)
-}
-
-FolderNode.prototype.exists = function (name) {
-  return !!this.get(name)
-}
-
-FolderNode.prototype.add = function (child) {
-  if (this.exists(child.name)) {
-    return false
+    return deserializeDir(serialized)
   }
 
-  if (child.parent) {
-    return false
+  getPath() {
+    let out = ''
+    let next = this
+
+    while (next) {
+      out = next.name + '/' + out
+      next = next.parent
+    }
+
+    return out
+  }
+}
+
+class FolderNode extends FSNode {
+  constructor(name) {
+    super(name, 'd')
+    this.children = []
   }
 
-  Object.defineProperty(child, 'parent', { value: this })
+  get(name) {
+    return this.children.find((node) => node.name === name)
+  }
 
-  this.children.push(child)
-  return true
+  exists(name) {
+    return !!this.get(name)
+  }
+
+  add(child) {
+    if (this.exists(child.name)) {
+      return false
+    }
+
+    if (child.parent) {
+      return false
+    }
+
+    Object.defineProperty(child, 'parent', { value: this })
+
+    this.children.push(child)
+    return true
+  }
+
+  remove(child) {
+    let index = this.children.findIndex((node) => node._id === child._id)
+    if (index > -1) this.children.splice(index, 1)
+  }
 }
 
-FolderNode.prototype.remove = function (child) {
-  let index = this.children.findIndex((node) => node._id === child._id)
-  if (index > -1) this.children.splice(index, 1)
-}
+class FileNode extends FSNode {
+  constructor(name, content) {
+    super(name, 'f')
+    this.content = content || ''
+  }
 
-function FileNode(name, content) {
-  // super()
-  FSNode.call(this, name, 'f')
+  erase() {
+    this.content = ''
+  }
 
-  this.content = content || ''
-}
+  write(content) {
+    this.content = content
+  }
 
-FileNode.prototype = new FSNode()
-
-FileNode.prototype.erase = function () {
-  this.content = ''
-}
-
-FileNode.prototype.write = function (content) {
-  this.content = content
-}
-
-FileNode.prototype.append = function (content) {
-  this.write(this.content + content)
+  append(content) {
+    this.write(this.content + content)
+  }
 }
 
 const Path = {
@@ -108,147 +106,147 @@ const Path = {
   join: (from, to) => from + (from.endsWith('/') ? '' : '/') + to,
 }
 
-function FS(root, pwd) {
-  this.root = root || new FolderNode('')
-  this.pwd = pwd || '/'
+export class FS {
+  constructor(root = new FolderNode(''), pwd = '/') {
+    this.root = root
+    this.pwd = pwd
 
-  if (!this.exists(this.pwd) || this.get(this.pwd).type !== 'd') {
-    this.pwd = '/'
-  }
-}
-
-FS.deserialize = function (serialized) {
-  if (!serialized) return new FS()
-
-  return new FS(FSNode.deserialize(serialized.root), serialized.pwd)
-}
-
-FS.prototype._resolve = function (path) {
-  let newPath = path
-  let pwd = this.pwd
-
-  if (newPath === '.') return pwd
-  if (newPath === '..') return Path.dirname(pwd)
-
-  if (newPath.startsWith('./')) newPath = newPath.substr(2)
-
-  while (newPath.startsWith('../')) {
-    newPath = newPath.substr(3)
-    pwd = Path.dirname(pwd)
+    if (!this.exists(this.pwd) || this.get(this.pwd).type !== 'd') {
+      this.pwd = '/'
+    }
   }
 
-  if (!newPath.startsWith('/')) {
-    newPath = Path.join(pwd, newPath)
+  static deserialize(serialized) {
+    if (!serialized) return new FS()
+
+    return new FS(FSNode.deserialize(serialized.root), serialized.pwd)
   }
 
-  return newPath
-}
+  _resolve(path) {
+    let newPath = path
+    let pwd = this.pwd
 
-FS.prototype.get = function (path) {
-  let realpath = this._resolve(path)
+    if (newPath === '.') return pwd
+    if (newPath === '..') return Path.dirname(pwd)
 
-  let split = realpath.substr(1).split('/')
-  let next = this.root
+    if (newPath.startsWith('./')) newPath = newPath.substr(2)
 
-  while (split.length) {
-    let nextName = split.shift()
-    if (!nextName) {
-      continue
+    while (newPath.startsWith('../')) {
+      newPath = newPath.substr(3)
+      pwd = Path.dirname(pwd)
     }
 
-    next = next.get(nextName)
-    if (!next) {
+    if (!newPath.startsWith('/')) {
+      newPath = Path.join(pwd, newPath)
+    }
+
+    return newPath
+  }
+
+  get(path) {
+    let realpath = this._resolve(path)
+
+    let split = realpath.substr(1).split('/')
+    let next = this.root
+
+    while (split.length) {
+      let nextName = split.shift()
+      if (!nextName) {
+        continue
+      }
+
+      next = next.get(nextName)
+      if (!next) {
+        return
+      }
+
+      if (split.length > 0 && next.type !== 'd') {
+        return
+      }
+    }
+
+    return next
+  }
+
+  exists(path) {
+    return !!this.get(path)
+  }
+
+  touch(path) {
+    if (this.exists(path)) {
       return
     }
 
-    if (split.length > 0 && next.type !== 'd') {
+    let parent = this.get(Path.dirname(this._resolve(path)))
+
+    if (!parent || !parent.type === 'd') {
       return
     }
+
+    parent.add(new FileNode(Path.basename(path)))
   }
 
-  return next
+  mkdir(path) {
+    if (this.exists(path)) {
+      return
+    }
+
+    let parent = this.get(Path.dirname(this._resolve(path)))
+
+    if (!parent || !parent.type === 'd') {
+      return
+    }
+
+    parent.add(new FolderNode(Path.basename(path)))
+  }
+
+  readDir(path) {
+    let dir = this.get(path)
+
+    if (!dir || dir.type !== 'd') {
+      return
+    }
+
+    return dir.children
+  }
+
+  readFile(path) {
+    let file = this.get(path)
+
+    if (!file || file.type !== 'f') {
+      return
+    }
+
+    return file.content
+  }
+
+  writeFile(path, content) {
+    let file = this.get(path)
+
+    if (!file || file.type !== 'f') {
+      return
+    }
+
+    file.write(content)
+  }
+
+  appendFile(path, content) {
+    let file = this.get(path)
+
+    if (!file || file.type !== 'f') {
+      return
+    }
+
+    file.append(content)
+  }
+
+  delete(path) {
+    let node = this.get(path)
+
+    if (!node || !node.parent) {
+      return
+    }
+
+    node.parent.remove(node)
+  }
 }
-
-FS.prototype.exists = function (path) {
-  return !!this.get(path)
-}
-
-FS.prototype.touch = function (path) {
-  if (this.exists(path)) {
-    return
-  }
-
-  let parent = this.get(Path.dirname(this._resolve(path)))
-
-  if (!parent || !parent.type === 'd') {
-    return
-  }
-
-  parent.add(new FileNode(Path.basename(path)))
-}
-
-FS.prototype.mkdir = function (path) {
-  if (this.exists(path)) {
-    return
-  }
-
-  let parent = this.get(Path.dirname(this._resolve(path)))
-
-  if (!parent || !parent.type === 'd') {
-    return
-  }
-
-  parent.add(new FolderNode(Path.basename(path)))
-}
-
-FS.prototype.readDir = function (path) {
-  let dir = this.get(path)
-
-  if (!dir || dir.type !== 'd') {
-    return
-  }
-
-  return dir.children
-}
-
-FS.prototype.readFile = function (path) {
-  let file = this.get(path)
-
-  if (!file || file.type !== 'f') {
-    return
-  }
-
-  return file.content
-}
-
-FS.prototype.writeFile = function (path, content) {
-  let file = this.get(path)
-
-  if (!file || file.type !== 'f') {
-    return
-  }
-
-  file.write(content)
-}
-
-FS.prototype.appendFile = function (path, content) {
-  let file = this.get(path)
-
-  if (!file || file.type !== 'f') {
-    return
-  }
-
-  file.append(content)
-}
-
-FS.prototype.delete = function (path) {
-  let node = this.get(path)
-
-  if (!node || !node.parent) {
-    return
-  }
-
-  node.parent.remove(node)
-}
-
-export { FS }
